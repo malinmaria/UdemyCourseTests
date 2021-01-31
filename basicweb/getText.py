396b411c69a392d8abf07eec67d782ccd2ed9b8d
malinmaria <malin.nemergut@gmail.com>
2021-01-31 23:56:28 +0000

    Getting text test

---
+++ basicweb/getText.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class GetText():
+
+    def test(self):
+        driver = webdriver.Firefox(executable_path="C:\\Users\\malin.nemergut\\PycharmProjects\\drivers\\geckodriver.exe")
+        baseURL = "https://letskodeit.teachable.com/pages/practice"
+        driver.maximize_window()
+        driver.implicitly_wait(10)
+        driver.get(baseURL)
+
+        openTabElement = driver.find_element(By.ID, "opentab")
+        elementText = openTabElement.text
+        print("Text on element is " + elementText)
+        time.sleep(1)
+        driver.quit()
+
+
+ff = GetText()
+ff.test()
