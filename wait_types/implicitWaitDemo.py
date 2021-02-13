f35a8b6b0c3739c0caedb580997789e8bbdca5aa
malinmaria <malin.nemergut@gmail.com>
2021-02-13 21:50:07 +0000

    Implicit wait demo class

---
+++ wait_types/implicitWaitDemo.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class ImplicitWaitDemo():
+    def test(self):
+        driver = webdriver.Firefox(executable_path="C:\\Users\\malin.nemergut\\PycharmProjects\\drivers\\geckodriver.exe")
+        baseURL = "https://letskodeit.teachable.com"
+        driver.maximize_window()
+        driver.get(baseURL)
+
+        loginLink = driver.find_element(By.XPATH, "//div[@id='navbar']//a[href='sign_in']")
+        loginLink.click()
+
+        emailField = driver.find_element(By.ID, "user_email")
+        emailField.send_keys("test")
+
+ff = ImplicitWaitDemo()
+ff.test()
