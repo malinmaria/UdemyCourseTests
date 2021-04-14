86777230514b66aac38a416fb03b904ff75f1266
malinmaria <malin.nemergut@gmail.com>
2021-04-14 23:37:13 +0100

    setting up first pytest scripts

--- pytest_package/test_case_demo2.py
+++ pytest_package/test_case_demo2.py
@@ -1,15 +1,14 @@
 import pytest
 
+
 @pytest.fixture()
 def setUp():
     print("Once before every method")
     yield
     print("Once after every method")
 
-
 def test_methodA(setUp):
     print("Running method A")
-
 
 def test_methodB(setUp):
     print("Running method B")
